2e3522b1c31e876133ac98b33171011d30a3a89c
Mounaimqc <160617744+Mounaimqc@users.noreply.github.com>
2026-05-30 16:49:36 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,28 @@
 // firebase-config.js
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+//import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+//const firebaseConfig = {
+//  apiKey: "AIzaSyCUcmmnvxjsZoDgQyESi5AvsynnH1kljFc",
+//  authDomain: "mounir-40df8.firebaseapp.com",
+//  projectId: "mounir-40df8",
+//  storageBucket: "mounir-40df8.firebasestorage.app",
+//  messagingSenderId: "855089551978",
+//  appId: "1:855089551978:web:dfd904eb3788d3504e4813",
+//  measurementId: "G-M680BM9Y77"
+//};
+
+//const app = initializeApp(firebaseConfig);
+//export const db = getFirestore(app);
+
+//console.log("✅ Firebase connecté avec succès!");
+
+
+
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js"; // ✅ أضف هذا
 
 const firebaseConfig = {
   apiKey: "AIzaSyCUcmmnvxjsZoDgQyESi5AvsynnH1kljFc",
@@ -14,9 +36,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-console.log("✅ Firebase connecté avec succès!");
-
-
-
-
+export const storage = getStorage(app); // ✅ صدّر storage
